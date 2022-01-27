e81def4ed6b4076ca13c1d98a8b3c46c52ef7046
Akin <akinkavak1@gmail.com>
2022-01-28 01:17:57 +0300

    Add Home and create About components

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,11 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+const About = () => (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+)
 
 
 function App() {
@@ -24,7 +29,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
